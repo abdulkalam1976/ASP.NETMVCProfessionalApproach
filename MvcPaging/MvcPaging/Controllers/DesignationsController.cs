30b3aa83d8d4c88e0039716afbf0965cef14284d
abdulkalam1976 <abdulkalam1976@gmail.com>
2017-04-24 15:04:56 +0300

    Implementing Single Paging view for a Project using ASP.NET MVC
    Implementing Single Paging view for a Project using ASP.NET MVC

---
+++ MvcPaging/MvcPaging/Controllers/DesignationsController.cs
@@ -0,0 +1,142 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using MvcPaging.Models;
+using PagedList;
+
+namespace MvcPaging.Controllers
+{
+    public class DesignationsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        public IPagedList DisplayFilterData(int? page, int? pagesize)
+        {
+            int pageSize = (pagesize ?? 3);
+            int pageNumber = (page ?? 1);
+
+            return db.Designations.OrderBy(m => m.Id).ToPagedList(pageNumber, pageSize);
+        }
+
+        // GET: Employees
+        public ActionResult Designations(int? page, int? pagesize)
+        {
+            TempData["CurrentAction"] = "Designations";
+            return View(DisplayFilterData(page, pagesize));
+        }
+
+        // GET: Employees
+        public ActionResult Index(int? page, int? pagesize)
+        {
+            return View();
+        }
+
+
+        // GET: Designations/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await db.Designations.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Designations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Code,Description")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Designations.Add(designation);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(designation);
+        }
+
+        // GET: Designations/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await db.Designations.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Code,Description")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(designation).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(designation);
+        }
+
+        // GET: Designations/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = await db.Designations.FindAsync(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Designation designation = await db.Designations.FindAsync(id);
+            db.Designations.Remove(designation);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcPaging/MvcPaging/Controllers/EmployeesController.cs
@@ -0,0 +1,133 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using MvcPaging.Models;
+using PagedList;
+
+namespace MvcPaging.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        public IPagedList DisplayFilterData(int? page, int? pagesize)
+        {
+            int pageSize = (pagesize ?? 10);
+            int pageNumber = (page ?? 1);
+
+            return db.Employees.OrderBy(m => m.Id).ToPagedList(pageNumber, pageSize);
+        }
+        // GET: Employees
+        public ActionResult Index(int? page, int? pagesize)
+        {
+            return View(DisplayFilterData(page, pagesize));
+        }
+        
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Employeecode,Employeename,Address")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Employeecode,Employeename,Address")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
